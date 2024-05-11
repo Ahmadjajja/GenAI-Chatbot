e8a8c858d03c5963d86ee4f03fb1974a496049a7
Ahmadjajja <ahmadjajja86@gmail.com>
2024-05-11 12:33:39 +0500

    12-Class33

---
+++ 4-Javascript/12-Class33/index.js
@@ -0,0 +1,55 @@
+// let num = [1, 3, 4, 2, 6, 5]
+
+// console.log(num)
+// // for (let index = 0; index < num.length; index++) {
+// //     num[index] = 1
+// // }
+// num.fill(1)
+// console.log(num)
+
+
+// console.log(new Date().getMinutes())
+
+// // map
+
+// let nums = [1, 3, 4, 2, 6, 5]
+// let doubledNums = nums.map(function (n) {
+//     return n * 3
+// })
+
+// console.log(
+//     " nums -> ", nums
+// );
+// console.log(
+//     "Double nums -> ", doubledNums
+// );
+
+// // filter
+
+// let nums = [1, 2, 4, 2, 6, 5]
+// let filteredNums = nums.filter(function (n) {
+//     // any logic can come at this place
+//     return n != 2
+// })
+
+// console.log(
+//     "nums -> ", nums
+// );
+// console.log(
+//     "Filtered nums -> ", filteredNums
+// );
+
+// find
+
+// let nums = [1, 2, 4, 2, 6, 5]
+// let foundValue = nums.find(function (n) {
+//     // any logic can come at this place
+//     return n != 6
+// })
+
+// console.log(
+//     "nums -> ", nums
+// );
+// console.log(
+//     "found value -> ", foundValue
+// );
