08c6e62c78e2a68e3c41202d34cf6e1154d69a5f
Ahmadjajja <ahmadjajja86@gmail.com>
2024-07-07 11:16:06 +0500

    25-Class46/

---
+++ 4-Javascript/25-Class46/revision.js
@@ -0,0 +1,60 @@
+// 1. Objects
+
+// Object: Combination of key value pairs. It contains properties(just line variable) and methods(just like functions).
+
+// let obj = {
+//     name: "hamza",
+//     age: "24"
+// }
+
+// console.log(Object.values(obj))
+// console.log(Object.keys(obj))
+// console.log(obj.name)
+// console.log(obj.age)
+
+// for (const key in obj) {
+//     console.log(obj[key])
+// }
+
+// 2. JSON
+// 3. Numbers
+// console.log(Number(new Date()))
+// console.log(parseInt("3000 C") * 2)
+// console.log(Number("3000 C") * 2)
+// 4. Math
+// 5. Strings
+
+let str = `ab${5}c`
+console.log(str[2])
+// 6. Arrays
+
+// let arr = [3, 5, 3]
+// console.log("original array: ", arr)
+// console.log("original array: ", arr.toString())
+// console.log("original array: ", arr.join(" - "))
+
+// let newArr = arr.map(function (item) {
+//     return item * 3
+// })
+// console.log(    "new array: ", newArr)
+
+// let filteredArr = arr.filter(function (item) {
+//     return item < 6
+// })
+// console.log("filteredArr array: ", filteredArr)
+
+// let foundElement = arr.find(function (item) {
+//     return item < 6
+// })
+// console.log("foundElement: ", foundElement)
+
+// let every = arr.every(function (item) {
+//     return item < 6
+// })
+// console.log("every: ", every)
+
+
+
+// 7. Data Types
+// 8. Dates
+// 9. Events
