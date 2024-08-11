ded393e8a9d2f8809c22d12a99c178a86d342375
Ahmadjajja <ahmadjajja86@gmail.com>
2024-08-11 17:43:54 +0500

    36-Class1 react.js introduction

---
+++ 5-React.js/36-Class1/first-react-app/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+function App() {
+
+  return (
+    <>
+
+      {/* Navbar */}
+      <h1>Navbar of our web page</h1>
+      {/* Main page */}
+      <h1>Main page of our web page</h1>
+      {/* Footer */}
+      <h1>Footer of our web page</h1>
+
+    </>
+  );
+}
+
+export default App;
