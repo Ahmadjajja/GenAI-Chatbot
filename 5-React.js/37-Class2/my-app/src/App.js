02c872f373e67dab7ed25b4babafe94efe0e3262
Ahmadjajja <ahmadjajja86@gmail.com>
2024-08-17 17:54:00 +0500

    React.js Class2

---
+++ 5-React.js/37-Class2/my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div>
+      <h1>Hello Everyone!</h1>
+      <h2>We are learning react.js</h2>
+    </div>
+  );
+  
+}
+
+export default App;
